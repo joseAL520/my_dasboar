6c4a14b8a03551deaf79c04bd6ebefb78432ed44
jose angel <jose.albelt1817@gmail.com>
2024-06-18 15:19:11 -0500

    defer views  - defer  de la Version 18

--- src/app/dasboard/pages/defer-views/defer-views.component.ts
+++ src/app/dasboard/pages/defer-views/defer-views.component.ts
@@ -1,17 +1,19 @@
 import { CommonModule } from '@angular/common';
 import {  Component } from '@angular/core';
+import { HeavyLoaderSlowComponent } from '../../shared/heavy-loader/heavy-loader-slow.component';
 @Component({
   selector: 'defer-views',
   standalone: true,
   imports: [
     CommonModule,
+    HeavyLoaderSlowComponent
   ],
   templateUrl:'defer-views.component.html',
-  styles: `
-    :host {
-      display: block;
-    }
-  `,
+ 
  
 })
-export default class DeferViewsComponent { }
+export default class DeferViewsComponent { 
+
+
+
+}
--- src/app/dasboard/shared/heavy-loader/heavy-loader-slow.component.ts
+++ src/app/dasboard/shared/heavy-loader/heavy-loader-slow.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loader-slow',
@@ -7,6 +7,23 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   imports: [
     CommonModule,
   ],
-  template: `<p>heavy-loader-slow works!</p>`,
+  template: `
+   <section [ngClass]="['w-full h-[600px]', cssClass]">
+     Heavy Loader Slow
+   </section>
+  
+  `,
 })
-export class HeavyLoaderSlowComponent { }
+export class HeavyLoaderSlowComponent {
+
+  @Input({ required:true }) cssClass!: string;
+
+
+
+  constructor(){
+    const start = Date.now();
+    while(Date.now() - start < 3000 ){}
+    console.log('cargado');
+  }
+
+}
