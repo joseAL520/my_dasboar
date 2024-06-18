3729bc04a5a12cc4c1f7659e12ddea528df424d0
jose angel <jose.albelt1817@gmail.com>
2024-06-18 14:39:07 -0500

    requerid @input de la Version 18

--- src/app/dasboard/pages/control-flow/control-flow.component.ts
+++ src/app/dasboard/pages/control-flow/control-flow.component.ts
@@ -1,16 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { TitleComponent } from "../../shared/title/title.component";
 
 
 type Grade = 'A'| 'B'| 'F'
 
 @Component({
-  selector: 'control-flow',
-  standalone: true,
-  imports: [
-    CommonModule,
-  ],
-  templateUrl:'control-flow.component.html',
+    selector: 'control-flow',
+    standalone: true,
+    templateUrl: 'control-flow.component.html',
+    imports: [
+        CommonModule,
+        TitleComponent
+    ]
 })
 export default class ControlFlowComponent { 
 
