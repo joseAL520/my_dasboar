03be66bd91461600e09ccaf664e1c52d77b1a351
jose angel <jose.albelt1817@gmail.com>
2024-06-18 14:58:03 -0500

    change detection -  onpush de la Version 18

--- src/app/dasboard/pages/change-detection/change-detection.component.ts
+++ src/app/dasboard/pages/change-detection/change-detection.component.ts
@@ -1,18 +1,44 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { TitleComponent } from "../../shared/title/title.component";
 
 @Component({
-  selector: 'change-detection',
-  standalone: true,
-  imports: [
-    CommonModule,
-  ],
-  templateUrl:'change-detection.component.html',
-  styles: `
-    :host {
-      display: block;
-    }
-  `,
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'change-detection',
+    standalone: true,
+    templateUrl: 'change-detection.component.html',
+    imports: [
+        CommonModule,
+        TitleComponent
+    ],
+
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ChangeDetectionComponent { }
+export default class ChangeDetectionComponent {
+
+  public frameworkSignal = signal({
+    name: 'angular',
+    releaseDate: 2016
+  });
+
+  public frameworkAsProperty = {
+    name: 'angular',
+    releaseDate: 2016
+  }
+
+
+  constructor(){
+
+    setTimeout(() => {
+      // this.frameworkAsProperty.name = 'react';
+
+
+      this.frameworkSignal.update( value => ({...value,name:'react'}))
+  
+      console.log('hecho');
+    }, 3000);
+
+
+  }
+
+
+}
