9cf8b510fbff12a59ceb5c530d045ef59d652c8a
jose angel <jose.albelt1817@gmail.com>
2024-06-18 09:49:43 -0500

    menu side menu - for de la nueva version

--- src/app/dasboard/dasboard.component.ts
+++ src/app/dasboard/dasboard.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SideMenuComponent } from './shared/sideMenu/sideMenu.component';
 
 @Component({
   selector: 'app-dasboard',
   standalone: true,
   imports: [
     RouterModule,
+    SideMenuComponent
   ],
   templateUrl:'dasboard.component.html',
   styles: `
--- src/app/dasboard/shared/sideMenu/sideMenu.component.ts
+++ src/app/dasboard/shared/sideMenu/sideMenu.component.ts
@@ -1,14 +1,38 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { routes } from '../../../app.routes';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-side-menu',
   standalone: true,
   imports: [
     CommonModule,
+    RouterModule
   ],
-  template: `<p>sideMenu works!</p>`,
+  templateUrl:'sideMenu.component.html',
   styleUrl: './sideMenu.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SideMenuComponent { }
+export class SideMenuComponent {
+
+  public menuItems = routes
+    .map(route => route.children ?? [])
+    .flat()
+    .filter(route => route && route.path )
+    .filter(route => !route.path?.includes(':') )
+
+
+  constructor(){
+    //  const dasboarRouter = routes
+    //  .map(route => route.children ?? [])
+    //  .flat()
+    //  .filter(route => route && route.path )
+    //  .filter(route => !route.path?.includes(':') )
+
+    //  console.log(dasboarRouter);
+  }
+
+
+
+}
